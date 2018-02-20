0d634c9ccd2c0c57b8bcfe1fe24ba63feee5e895
Yetty mama <yetunde95@yahoo.com>
2018-02-20 21:40:50 +0000

    Another irrelevant commit

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-        <title>Techit Demo</title>
+        <title>Techit Demo - Remove Later</title>
 
         <?php
         error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE);
